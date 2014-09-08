0d0c17b5000dfa9966209018c07de9d88f58a21e
Jeff Crocker <crockj72@gmail.com>
2014-09-07 23:38:02 -0700

    #define MAX_INCLUDE_DEPTH 10

--- 120++.c
+++ 120++.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 extern int yylex();
+extern FILE* yyin;
+extern FILE* yyout;
 
 int main(int argc, char** argv) {
-    return 0;
+    int _ = 10;
+    return _;
 }
