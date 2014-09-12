3459508104fbce8db50d63bbe50ded498369c3dc
Jeff Crocker <crockj72@gmail.com>
2014-09-12 12:38:39 -0700

    downloaded examples

--- examples/A.h
+++ examples/A.h
@@ -4,4 +4,4 @@ private:
 public:
    // given a scale, return the sum of the scaled components
    double scaledsum(double);
-};
+   };
--- examples/C.cpp
+++ examples/C.cpp
@@ -1,6 +1,5 @@
 // #include "A.h" not needed here, B.h include it
+#include "B.h"
 int main()
 {
-    float pi = 3.14;
-    return 0;
 }
