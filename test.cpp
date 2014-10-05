1e82fb78bb9d5d4f8fbbe999f7fc054ff02af4b8
Jeff Crocker <crockj72@gmail.com>
2014-10-05 14:51:28 -0700

    namespace lexer improved

--- test.cpp
+++ test.cpp
@@ -1,3 +1,16 @@
-int main() {
-    foo
+ 
+#include <iostream>
+
+typedef struct grog { };
+
+class baz {
+public:
+    void grock(char*);
+    int x;
+}
+
+void foo();
+
+int main(int argc, char** argv) {
+    int x = 1;
 }
