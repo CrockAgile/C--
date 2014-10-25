4e8fd6e31a099307ddfb327bbb92001b1133ce20
Jeff Crocker <crockj72@gmail.com>
2014-10-24 23:17:01 -0700

    Modular Command Line Flags, memory leaks fixed

--- 120++.c
+++ 120++.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "parse.h"
 
@@ -14,20 +15,23 @@ extern FILE* yyout;
 void call_parsing(char*,int);
 
 int main(int argc, char** argv) {
-    int i,print_mode = 0;
+    int i,c,index,print_mode = 0;
+    opterr = 0;
+    while ((c = getopt (argc, argv, "v")) != -1)
+        switch(c) {
+            case 'v':
+                print_mode = 1;
+                break;
+        }
+
     if (!init_nametable()){
         fprintf(stderr,"Failed to initialize nametable\n");
         exit(1);
     }
+    for (index=optind; index < argc; index++)
+        call_parsing(argv[index],print_mode);
     
-    for (i=1; i<argc; i++) {
-        if (strcmp(argv[i], "-v")) {
-            call_parsing(argv[i],print_mode);
-        }
-        else {
-            print_mode = 1;
-        }
-    }
+    free_nametable();
     return 0;
 }
 
@@ -39,9 +43,9 @@ void call_parsing(char* filename,int print_mode) {
     if( (parse_result == 0 ) && print_mode) {
         printf("\n***%s***\n",filename);
         treeprint(root,0);
-        freetree(root);
-        free_nametable();
     } else if ( parse_result == 1 ) {
         exit(2);
     }
+    freetree(root);
+    fclose(yyin);
 }
